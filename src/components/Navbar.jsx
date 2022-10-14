8e2a2414aa819cdc0d8b48817717c1a3291db449
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-14 14:20:04 -0400

    Nick's Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+// try {
+//   const response = await fetch(
+//     'https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players'
+//   );
+//   const result = await response.json();
+//   console.log(result);
+// } catch (err) {
+//   console.error(err);
+// }
+
+
 
 const Navbar = () => {
   return (
     <div id="navbar">
-    <h2>I am navbar</h2>
+    <form >
+      <span>Name: </span><input type='text' id='Name' name='Name' placeholder='Name'></input>
+      <span>Breed: </span><input type='text' id='Breed' name='Breed' placeholder='Breed'></input>
+      <input type='button' id='submit' name='submit' value='submit'></input>
+    </form>
   </div>
   );
 };
---
+++ src/components/PuppyRoster.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PuppyRoster = () => {
+
+    return (
+        <h2>this is puppy roster</h2>
+        // <div id="Roster">
+        //     <div id="Puppy" name="Puppy"></div>
+        //     <div id="Puppy" name="Puppy"></div>
+        //     <div id="Puppy" name="Puppy"></div>
+        // </div>
+
+    )
+}
+
+
+
+
+
+
+
+
+export default PuppyRoster;
